2d7bb1a9f03b0e500d426a4e71b77beac9118f98
Trent Hashimoto <t.hashiphoto@gmail.com>
2018-11-29 06:11:47 -0800

    Create Walls

---
+++ src/gameElements/Wall.java
@@ -0,0 +1,35 @@
+package gameElements;
+
+public class Wall {
+	public int x1;
+	public int x2;
+	public int y1;
+	public int y2;
+	
+	public Wall(int x1, int x2, int y1, int y2) {
+		if(x1 < x2) {
+			this.x1 = x1;
+			this.x2 = x2;
+		} 
+		else {
+			this.x2 = x1;
+			this.x1 = x2;
+		}
+		if(y1 < y2) {
+			this.y1 = y1;
+			this.y2 = y2;
+		} 
+		else {
+			this.y2 = y1;
+			this.y1 = y2;
+		}
+	}
+	
+	public int width() {
+		return x2 - x1;
+	}
+	
+	public int height() {
+		return y2 - y1;
+	}
+}
